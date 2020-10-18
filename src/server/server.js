8afd6a1133a2711394a8080807d14d648647e831
Ashley Smith <ash.mash.134@gmail.com>
2020-10-19 10:18:31 +1100

    Added schema for Pins and started utils for pins

--- src/server/server.js
+++ src/server/server.js
@@ -1,50 +1,57 @@
-const express = require('express')
+const express = require('express');
 const expressSession = require('express-session');
 const MongoStore = require('connect-mongo')(expressSession);
 const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
 
-const userRouter = require('./../routes/users_routes')
-const pageRouter = require('./../routes/pages_routes')
-const authRouter = require('./../routes/auth_routes')
+const userRouter = require('./../routes/users_routes');
+const pageRouter = require('./../routes/pages_routes');
+const authRouter = require('./../routes/auth_routes');
 
-const app = express()
-app.use(expressSession({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { expires: 600000},
-  store: new MongoStore({ mongooseConnection: mongoose.connection })
-}))
+const app = express();
+app.use(
+  expressSession({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { expires: 600000 },
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
 
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}))
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-const dbConn = 'mongodb://127.0.0.1:27017/caculocation_db'
-mongoose.connect(dbConn, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false
-},
-(err) => {
-  if (err) {
+const dbConn = 'mongodb://127.0.0.1:27017/caculocation_db';
+mongoose.connect(
+  dbConn,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  },
+  (err) => {
+    if (err) {
       console.log('Error connecting to database', err);
-  } else {
+    } else {
       console.log('Connected to database!');
+    }
   }
-});
+);
 
-app.engine('handlebars', exphbs())
-app.set('view engine', 'handlebars')
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 app.use('/', pageRouter);
 app.use('/dashboard', userRouter);
 app.use('/user', authRouter);
 
-const port = process.env.port || 3007
+const port = process.env.port || 3007;
 
 app.listen(port, () => {
- console.log(`app listening on port ${port}`)
-})
+  console.log(`app listening on port ${port}`);
+});
